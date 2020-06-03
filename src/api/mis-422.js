7acccbae9a4717ae9bc35ee11ec79303e0d6218a
ahmetrukenboyaci <ahmetrukenboyaci@gmail.com>
2020-06-03 17:37:22 +0300

    .

--- src/api/mis-422.js
+++ src/api/mis-422.js
@@ -1,7 +1,22 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import {getCookie, setCookie} from "../utils/cookie";
 
-export default axios.create({
+
+let axiosInstance = axios.create({
   baseURL: "https://mis-422.herokuapp.com",
   headers: { "Content-Type": "application/json", authorization: "Bearer " + getCookie("token") },
 });
+
+axiosInstance.interceptors.response.use((response) => response, async (error) => {
+  if (error.response.status === 401) {
+    let password = new Buffer(getCookie('userid'), 'base64').toString('ascii');
+    let username = new Buffer(getCookie('user'), 'base64').toString('ascii');
+    let tokenRes = await axiosInstance.post("/api/authenticate", {username, password});
+    setCookie("token", tokenRes.data.id_token, {});
+    window.location.reload();
+  } else {
+    return error;
+  }
+});
+
+export default axiosInstance;
